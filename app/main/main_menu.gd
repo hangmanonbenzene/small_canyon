b0154aa7f98ec0ae9191d13067e89eece36897b2
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-10-16 12:15:54 +0200

    menus done

--- app/main/main_menu.gd
+++ app/main/main_menu.gd
@@ -1,14 +1,26 @@
-extends Control
+extends Node
 
-@export var main_menu_buttons: Control
+@export var main_menu: Control
+@export var level_menu: Control
+@export var editor_menu: Control
 
 func _on_level_button_pressed() -> void:
-	main_menu_buttons.visible = false
+	main_menu.visible = false
+	level_menu.visible = true
+	editor_menu.visible = false
 
 
 func _on_editor_button_pressed() -> void:
-	main_menu_buttons.visible = false
+	main_menu.visible = false
+	level_menu.visible = false
+	editor_menu.visible = true
 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_back_pressed() -> void:
+	main_menu.visible = true
+	level_menu.visible = false
+	editor_menu.visible = false
