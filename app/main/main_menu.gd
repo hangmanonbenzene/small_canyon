2028e0f14e05f90ce029da12fe5eeae147b1a9b0
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-10-18 13:40:40 +0200

    main menu second iteration

--- app/main/main_menu.gd
+++ app/main/main_menu.gd
@@ -1,28 +1,15 @@
 extends Node
 
 @export var main_menu: Control
-@export var level_menu: Control
-@export var editor_menu: Control
 
 func _on_level_button_pressed() -> void:
 	main_menu.visible = false
-	level_menu.visible = true
-	editor_menu.visible = false
 
 func _on_editor_button_pressed() -> void:
 	main_menu.visible = false
-	level_menu.visible = false
-	editor_menu.visible = true
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
 
-func _on_back_pressed() -> void:
-	main_menu.visible = true
-	level_menu.visible = false
-	editor_menu.visible = false
-
 func _on_new_button_pressed() -> void:
 	main_menu.visible = false
-	level_menu.visible = false
-	editor_menu.visible = false
