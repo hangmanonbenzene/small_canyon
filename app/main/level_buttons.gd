ac016ba938b2629c7568e34c3251dee868ecc9f4
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-10-20 18:08:42 +0200

    level list base

---
+++ app/main/level_buttons.gd
@@ -0,0 +1,28 @@
+extends HBoxContainer
+
+@export var name_button: Button
+var level_name: String = "###ERROR###"
+var menu: Node
+
+func set_up(new_menu: Node, new_name: String) -> void:
+	menu = new_menu
+	level_name = new_name
+	name_button.text = new_name
+
+func _on_play_pressed() -> void:
+	if menu.has_method("_on_play_level_pressed"):
+		menu._on_play_level_pressed(self, level_name)
+	else:
+		level_name = "###ERROR###"
+
+func _on_edit_pressed() -> void:
+	if menu.has_method("_on_edit_level_pressed"):
+		menu._on_edit_level_pressed(self, level_name)
+	else:
+		level_name = "###ERROR###"
+
+func _on_delete_pressed() -> void:
+	if menu.has_method("_on_delete_level_pressed"):
+		menu._on_delete_level_pressed(self, level_name)
+	else:
+		level_name = "###ERROR###"
--- app/main/main_menu.gd
+++ app/main/main_menu.gd
@@ -1,15 +1,25 @@
 extends Node
 
 @export var main_menu: Control
-
-func _on_level_button_pressed() -> void:
-	main_menu.visible = false
-
-func _on_editor_button_pressed() -> void:
-	main_menu.visible = false
-
-func _on_exit_button_pressed() -> void:
-	get_tree().quit()
+@export var level_buttons: Control
+@export var level_button: PackedScene
 
 func _on_new_button_pressed() -> void:
 	main_menu.visible = false
+
+func _on_play_level_pressed(button: Control, level_name: String) -> void:
+	print("play " + level_name)
+	main_menu.visible = false
+
+func _on_edit_level_pressed(button: Control, level_name: String) -> void:
+	print("edit " + level_name)
+	main_menu.visible = false
+
+func _on_delete_level_pressed(button: Control, level_name: String) -> void:
+	print("delete " + level_name)
+	button.queue_free()
+
+func load_levels() -> void:
+	var new_button: Control = level_button.instantiate()
+	new_button.set_up(self, "test")
+	level_buttons.add_child(new_button)
