9ecceecc98a010a9060bd2cd677fbf719450fd57
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-10-21 16:14:27 +0200

    removed test

--- test/level_stuff/character.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends Node3D
-
-@export var priority: int:
-	set(value):
-		priority = value
-		$Node3D/MeshInstance3D.material_override.render_priority = value
-		$Node3D/MeshInstance3D2.material_override.render_priority = value
