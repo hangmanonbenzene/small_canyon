2aed175204e15b1c76cf77f30e369930580d28e6
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-10-27 19:45:51 +0100

    clickable blocks

--- app/blocks/cube.gd
+++ app/blocks/cube.gd
@@ -5,4 +5,8 @@ extends Node3D
 	set(value):
 		side.get_surface_override_material(0).albedo_color = value
 		side_color = value
-		print("test")
+
+
+func _on_area_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+		print("yay")
