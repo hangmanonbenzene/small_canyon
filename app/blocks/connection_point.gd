52f94d3d5c76fa019342b50d9fa05b8d1e5c1111
hangman_on_benzene <98060908+hangmanonbenzene@users.noreply.github.com>
2025-11-19 17:21:43 +0100

    Refactor raycast logic into Main.raycast method
    Moved raycast logic from ConnectionPoint to a new static Main.raycast method for reuse and clarity. ConnectionPoint now uses Main.raycast to determine viable directions.

--- app/blocks/connection_point.gd
+++ app/blocks/connection_point.gd
@@ -6,14 +6,4 @@ var depth: int
 var block_behind_this: ConnectionPoint
 
 func viable_direction(direction: Vector3) -> bool:
-	var start_pos: Vector3 = Main.get_position(self)
-	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	var end_pos: Vector3 = start_pos + (direction / 2)
-	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(start_pos, end_pos)
-	query.exclude = [self]
-	query.collide_with_areas = true
-	query.collide_with_bodies = false
-	query.collision_mask = 1
-	var result: Dictionary = space_state.intersect_ray(query)
-	if result: return true
-	else: return false
+	return not Main.raycast(self, direction, 1).is_empty()
--- app/main/main.gd
+++ app/main/main.gd
@@ -12,3 +12,14 @@ static func vector3_to_vector3i(vector: Vector3) -> Vector3i:
 
 static func get_position(node: Node3D) -> Vector3i:
 	return vector3_to_vector3i(node.global_position)
+
+static func raycast(start: CollisionShape3D, direction: Vector3, mask: int) -> Dictionary:
+	var start_pos: Vector3 = Main.get_position(start)
+	var space_state: PhysicsDirectSpaceState3D = start.get_world_3d().direct_space_state
+	var end_pos: Vector3 = start_pos + (direction.normalized() / 2)
+	var query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(start_pos, end_pos)
+	query.exclude = [start]
+	query.collide_with_areas = true
+	query.collide_with_bodies = false
+	query.collision_mask = mask
+	return space_state.intersect_ray(query)
